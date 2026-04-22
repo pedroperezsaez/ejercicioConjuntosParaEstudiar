a0b6a01d881b3dcb9f8679b2ab60f4c45ac9f58a
pedroperezsaez <pedroperezsaez.1992@gmail.com>
2026-04-22 19:10:27 +0200

    haciendo

--- src/Exercici3/ex3.java
+++ src/Exercici3/ex3.java
@@ -1,11 +1,13 @@
 package Exercici3;
 
+import java.util.Comparator;
+
 public class ex3 {
     public static void main(String[] args) {
 
     }
 }
-class Person {
+class Person implements Comparator<Person> {
     int birthYear;
     String name;
     Person(String name, int birthYear) {
@@ -15,5 +17,15 @@ class Person {
     @Override
     public String toString() {
         return String.format("%s %d", name, birthYear);
+    }
+
+
+    @Override
+    public int compare(Person p1, Person p2) {
+
+    }
+
+    public int PersonBirthComparator(Person p1, Person p2) {
+
     }
 }
